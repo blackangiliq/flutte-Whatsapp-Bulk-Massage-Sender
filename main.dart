5e98c427f49df9b4a72b04514f71f75a1e7d73c8
B14ck <52348159+blackangiliq@users.noreply.github.com>
2022-07-01 11:22:00 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(What());
+}
+
+class What extends StatefulWidget {
+  const What({Key? key}) : super(key: key);
+
+  @override
+  State<What> createState() => _WhatState();
+}
+
+class _WhatState extends State<What> {
+  TextEditingController MsgController = TextEditingController();
+  TextEditingController NumberListControler = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: ListView(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(controller: MsgController),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              controller: NumberListControler,
+              maxLines: 20,
+            ),
+          ),
+          FloatingActionButton(
+            onPressed: () async {
+              List Numbers = NumberListControler.text.split('\n');
+
+              await Future.forEach(Numbers, (element) async {
+                await Future.delayed(const Duration(seconds: 2));
+                var startWhatsup = await Process.start('cmd', ['']);
+
+                startWhatsup.stdin.writeln(
+                    'start whatsapp://send?phone=964$element^&text=${MsgController.text}');
+
+                var snedenter = await Process.start(
+                    'cscript.exe', ['C:/Users/<ur pc username >/Desktop/sendenter.vbs']);
+              });
+            },
+            child: Icon(Icons.send),
+          )
+        ],
+      )),
+    );
+  }
+}
+
+// var startWhatsup = await Process.start('cmd', ['']);
+//
+// startWhatsup.stdin.writeln('start whatsapp://send?phone=9647729379356^&text=hi');
+//
+// var snedenter = await Process.start('cscript.exe', ['C:/Users/plack/Desktop/1.vbs']);
+//
---
+++ sendenter.vbs
@@ -0,0 +1,6 @@
+Set WshShell = WScript.CreateObject("WScript.Shell")
+
+WScript.Sleep 1000 
+' Type ENTER
+
+WshShell.SendKeys "{ENTER}"
